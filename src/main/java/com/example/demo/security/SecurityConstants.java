d571f9b9d8af71c97b12505f45efcc630c37c7c4
paul156551 <pavel.ryzhkov.99@mail.ru>
2022-03-25 17:47:45 +0300

    Initial Commit

--- src/main/java/com/example/demo/security/SecurityConstants.java
+++ src/main/java/com/example/demo/security/SecurityConstants.java
@@ -1,8 +1,8 @@
 package com.example.demo.security;
 
 public class SecurityConstants {
-    public static final String SIGN_UP_URLS = "api/auth/**";
-    public static final String SECRET_KEY = "";
+    public static final String SIGN_UP_URLS = "/api/auth/**";
+    public static final String SECRET_KEY = "Cat";
     public static final String TOKEN_PREFIX= "Bearer";
     public static final String HEADER_STRING= "Authorization";
     public static final String CONTENT_TYPE= "application/json";
